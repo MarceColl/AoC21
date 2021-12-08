b29d165455e926e5cc7eef1cca07bebe2e749ce4
Marce Coll <marce@dziban.net>
2021-12-08 13:13:17 +0100

    day8bis reduced code

--- day8bis.py
+++ day8bis.py
@@ -1,6 +1,3 @@
-import sys
-import itertools
-
 #  1111
 # 2    3
 # 2    3
@@ -10,91 +7,42 @@ import itertools
 #  7777
 
 num_to_segments = {
-    1: {3, 6},
-    2: {1,3,4,5,7},
-    3: {1,3,4,6,7},
-    4: {2,3,4,6},
-    5: {1,2,4,6,7},
-    6: {1,2,4,5,6,7},
-    7: {1,3,6},
-    8: {1,2,3,4,5,6,7},
-    9: {1,2,3,4,6,7},
-    0: {1,2,3,5,6,7},
+    1: {3, 6}, 2: {1,3,4,5,7},
+    3: {1,3,4,6,7}, 4: {2,3,4,6},
+    5: {1,2,4,6,7}, 6: {1,2,4,5,6,7},
+    7: {1,3,6}, 8: {1,2,3,4,5,6,7},
+    9: {1,2,3,4,6,7}, 0: {1,2,3,5,6,7},
 }
 
 def find_freq(seg):
-    values = {v for s in seg for v in s}
-    freq = {}
+    values, freq = {v for s in seg for v in s}, {}
     for v in values:
-        freq[v] = 0
-
-        for s in seg:
-            if v in s:
-                freq[v] += 1
+        freq[v] = sum([1 for s in seg if v in s])
     return freq
-
-num_freq = find_freq(num_to_segments.values())
-print(num_freq)
-
-class SegDisp:
-    def __init__(self, d):
-        self.segment = [set() for i in range(7)]
-        self.mapping = dict()
-        self.d = d
-        self.d_freq = find_freq(self.d)
-        self.segs_to_nums = {}
-
-    def gbl(self, l):
-        return set([n for n in self.d if len(n) == l][0])
-
-    def gbf(self, f):
-        return {n for n, nf in self.d_freq.items() if nf == f}
-
-    def sd(d, s):
-        pass
-
-    def solve(self):
-        disp1 = self.gbl(2)
-        disp7 = self.gbl(3)
-        seg1 = disp7 - disp1
-        seg2 = self.gbf(6)
-        seg3 = self.gbf(8) - seg1
-        seg47 = self.gbf(7)
-        seg5 = self.gbf(4)
-        seg6 = self.gbf(9)
-
-        d4 = self.gbl(4)
-        seg4 = seg47 - d4
-        seg7 = seg47 - seg4
-
-        segs = [seg1, seg2, seg3, seg7, seg5, seg6, seg4]
-        segs = [list(s)[0] for s in segs]
-
-        self.seg_to_nums = {}
-        for num, numsegs in num_to_segments.items():
-            segset = frozenset({segs[n-1] for n in numsegs})
-            self.seg_to_nums[segset] = num
-
-    def translate(self, l):
-        num = [self.seg_to_nums[frozenset(e)] for e in l]
-        return sum([n*pow(10,i) for i, n in enumerate(reversed(num))])
-
-inp = ["gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc"]
-val = ["fgae", "cfgab", "fg", "bagce"]
 
 with open('day8in', 'r') as f:
     inp = f.readlines()
 
-acc = 0
-for l in inp:
-    inp, val = l.strip().split(' | ')
-    inp = inp.split(' ')
-    val = val.split(' ')
+def solve(inp, val):
+    inp_freq = find_freq(inp)
 
-    d = SegDisp(inp)
-    d.solve()
-    acc += d.translate(val)
+    def l(_l):
+        return set([n for n in inp if len(n) == _l][0])
 
-print(acc)
+    def f(_f):
+        return {n for n, nf in inp_freq.items() if nf == _f}
 
+    d1, d4, d7, s47 = l(2), l(4), l(3), f(7)
+    s1 = d7 - d1
+    segs = [list(s)[0] for s in [s1, f(6), f(8) - s1, s47 - (f(7) - d4), f(4), f(9), s47 - d4]]
+
+    s2n = {}
+    for num, numsegs in num_to_segments.items():
+        segset = frozenset({segs[n-1] for n in numsegs})
+        s2n[segset] = num
+
+    num = [s2n[frozenset(e)] for e in val]
+    return sum([n*pow(10,i) for i, n in enumerate(reversed(num))])
+
+print(sum([solve(i.split(' '), val.split(' ')) for l in inp for i, val in [l.strip().split(' | ')]]))
 
