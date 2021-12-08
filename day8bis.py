67c942a02b0912ff9a904dfae1e0e15f8e751efb
Marce Coll <marce@dziban.net>
2021-12-08 13:28:04 +0100

    Even smaller day8bis

--- day8bis.py
+++ day8bis.py
@@ -6,41 +6,20 @@
 # 5    6
 #  7777
 
-num_to_segments = {
-    1: {3, 6}, 2: {1,3,4,5,7},
-    3: {1,3,4,6,7}, 4: {2,3,4,6},
-    5: {1,2,4,6,7}, 6: {1,2,4,5,6,7},
-    7: {1,3,6}, 8: {1,2,3,4,5,6,7},
-    9: {1,2,3,4,6,7}, 0: {1,2,3,5,6,7},
-}
-
-def find_freq(seg):
-    values, freq = {v for s in seg for v in s}, {}
-    for v in values:
-        freq[v] = sum([1 for s in seg if v in s])
-    return freq
+n2s={1:{3,6},2:{1,3,4,5,7},3:{1,3,4,6,7},4:{2,3,4,6},5:{1,2,4,6,7},6:{1,2,4,5,6,7},7:{1,3,6},8:{1,2,3,4,5,6,7},9:{1,2,3,4,6,7},0:{1,2,3,5,6,7}}
 
 with open('day8in', 'r') as f:
     inp = f.readlines()
 
+ff = lambda seg: {v: sum([1 for s in seg if v in s]) for v in {v for s in seg for v in s}}
 def solve(inp, val):
-    inp_freq = find_freq(inp)
-
-    def l(_l):
-        return set([n for n in inp if len(n) == _l][0])
-
-    def f(_f):
-        return {n for n, nf in inp_freq.items() if nf == _f}
-
+    inp_f = ff(inp)
+    l = lambda _l: set([n for n in inp if len(n) == _l][0])
+    f = lambda _f: {n for n, nf in inp_f.items() if nf == _f}
     d1, d4, d7, s47 = l(2), l(4), l(3), f(7)
     s1 = d7 - d1
     segs = [list(s)[0] for s in [s1, f(6), f(8) - s1, s47 - (f(7) - d4), f(4), f(9), s47 - d4]]
-
-    s2n = {}
-    for num, numsegs in num_to_segments.items():
-        segset = frozenset({segs[n-1] for n in numsegs})
-        s2n[segset] = num
-
+    s2n = { ss: n for n, ns in n2s.items() for ss in [frozenset({segs[n-1] for n in ns})] }
     num = [s2n[frozenset(e)] for e in val]
     return sum([n*pow(10,i) for i, n in enumerate(reversed(num))])
 
